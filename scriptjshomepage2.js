c43010704e8fa13d3e801da3fca150ee9a885001
Sanya Gupta <sanyagup07@gmail.com>
2024-04-12 12:33:21 -0400

    updated logic for final course grades

--- scriptjshomepage2.js
+++ scriptjshomepage2.js
@@ -10,6 +10,12 @@ function w3_close() {
     document.getElementById("openNav").style.display = "inline-block";
 }
 
+gradeLetterQ1="";
+gradeLetterQ2="";
+gradeLetterQ3="";
+gradeLetterQ4="";
+unweighted = 0;
+weighted = 0;
 
 function setGradeNumberQ1(letter) {
   gradeLetterQ1 = letter;
@@ -32,4 +38,13 @@ function setGradeNumberQ4(letter) {
 }
 
 
-
+const calcAssess = () => {
+  const qtrSum = [...document.querySelectorAll(".qtra")].reduce((x,y) => x + parseInt(y.value), 0);
+  let total = qtrSum * (1 / 5);
+  const qtr1 = document.getElementById("qtr1a").value;
+  const qtr2 = document.getElementById("qtr2a").value;
+  const qtr3 = document.getElementById("qtr3a").value;
+  const qtr4 = document.getElementById("qtr4a").value;
+  if ((parseInt(qtr3) + parseInt(qtr4) === 0) || ((parseInt(qtr1) + parseInt(qtr2) === 0) && (parseInt(qtr3) === 0 || parseInt(qtr4) === 0))) total = 0;
+  document.getElementById("gradea").innerText = total >= 3.5 ? 'A' : total >= 2.5 ? 'B' : total >= 1.5 ? 'C' : total >= 0.75 ? 'D' : 'E';
+}
